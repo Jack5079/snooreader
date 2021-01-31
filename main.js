6948563f848fe3dfffb0aa0d831c26fcb2c405dd
Jack <29169102+Jack5079@users.noreply.github.com>
2021-01-30 23:13:05 -0500

    aaa

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
+/* eslint-env browser */
 /**
- * 
+ *
  * @type {<Type>(arr: Type[]) => Type}
  */
 const random = (arr) => arr[Math.floor(Math.random() * arr.length)]
 function beep () {
   return new Promise((resolve) => {
     const gif = new Image()
-    gif.src = "assets/static.gif"
-    gif.classList.add("top")
+    gif.src = 'assets/static.gif'
+    gif.classList.add('top')
     document.body.append(gif)
     const audioContext = new AudioContext()
-    var bufferSize = 4096
-    var whiteNoise = audioContext.createScriptProcessor(bufferSize, 1, 1)
+    const bufferSize = 4096
+    const whiteNoise = audioContext.createScriptProcessor(bufferSize, 1, 1)
 
     whiteNoise.onaudioprocess = function (ev) {
-      var output = ev.outputBuffer.getChannelData(0)
-      for (var i = 0; i < bufferSize; i++) {
+      const output = ev.outputBuffer.getChannelData(0)
+      for (let i = 0; i < bufferSize; i++) {
         output[i] = Math.random() * 2 - 1
       }
     }
@@ -48,30 +49,30 @@ function say (text) {
 /**
  * @type {HTMLTemplateElement | null}
 */
-const template = document.querySelector("article#comment");
+const template = document.querySelector('article#comment');
 (async () => {
-  var [
+  let [
     { data: { children: [{ data: { title, author } }] } },
-    { data: { children } },
-  ] = await fetch("https://www.reddit.com/r/AskReddit/comments/i0orws/.json", {
-    cache: "force-cache",
+    { data: { children } }
+  ] = await fetch('https://www.reddit.com/r/AskReddit/comments/knvuq2/.json', {
+    cache: 'force-cache'
   })
     .then((res) => res.json())
   children = children.filter(comment => comment.data.body)
-  template.querySelector("h1").innerText = title
-  template.querySelector("a").innerText = author
-  template.querySelector("a").href = `https://reddit.com/u/${author}`
+  template.querySelector('h1').innerText = title
+  template.querySelector('a').innerText = author
+  template.querySelector('a').href = `https://reddit.com/u/${author}`
   await say(title)
-  template.querySelector("h1")?.remove()
+  template.querySelector('h1')?.remove()
   await beep()
   while (true) {
     const data = random(children)
-    children = children.filter(comment => comment != data)
+    children = children.filter(comment => comment !== data)
     const { data: comment } = data
     console.log(comment)
-    template.querySelector("p").innerText = comment.body
-    template.querySelector("a").innerText = comment.author
-    template.querySelector("a").href = `https://reddit.com/u/${comment.author}`
+    template.querySelector('p').innerText = comment.body
+    template.querySelector('a').innerText = comment.author
+    template.querySelector('a').href = `https://reddit.com/u/${comment.author}`
     await say(comment.body)
     await beep()
   }
